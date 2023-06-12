14c21ccff1f33e50af43f64aab648a084604f645
saurabhB-Webonise <saurabh.bhorkar@weboniselab.com>
2023-06-12 15:59:55 +0530

    refactor

--- src/components/todoitems/TodoItems.js
+++ src/components/todoitems/TodoItems.js
@@ -3,18 +3,18 @@ import './todoitems.css';
 import { FaTrash } from 'react-icons/fa';
 
 function TodoItems({ onCheck, onClick, data }) {
-    
+
     const checkHandler = () => {
         onCheck(data.id)
     }
-    
+
     const clickHandler = () => {
         onClick(data.id)
     }
 
     return <div className='todoitem' >
         <div className='todoItemWrapper'>
-            <input className='checkBox' type='checkbox' onChange={checkHandler} checked={data.completed}/>
+            <input className='checkBox' type='checkbox' onChange={checkHandler} checked={data.completed} />
             <label className='todoText'>{data.text}</label>
         </div>
         <span className='deleteButton'> <FaTrash onClick={clickHandler} /></span>
