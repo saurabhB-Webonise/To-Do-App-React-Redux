8441672215540dc65f6e0c60710e0104db972fff
saurabhB-Webonise <saurabh.bhorkar@weboniselab.com>
2023-06-23 13:09:58 +0530

    length check

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -4,5 +4,5 @@ import { Navigate } from 'react-router-dom';
 
 export default function PrivateRoute({ children }) {
     const { data } = useSelector((state) => state.api);
-    return (Array.isArray(data)) ?  <Navigate to="/" replace={true}/> :children
+    return (Array.isArray(data) && data.length === 0) ? <Navigate to="/" replace={true} /> : children
 }
