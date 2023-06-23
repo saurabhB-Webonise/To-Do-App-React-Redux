1fb934c3f58f49c88efae130f2e9c9d52026744d
saurabhB-Webonise <saurabh.bhorkar@weboniselab.com>
2023-06-24 00:46:56 +0530

    env changes

--- src/constants/api-constants.js
+++ src/constants/api-constants.js
@@ -1,4 +1,4 @@
-export const DOMAIN = "https://dummyjson.com";
+export const DOMAIN = process.env.REACT_APP_DOMAIN_URL
 export const LOGIN = "/auth/login";
 export const USER_RECORDS = "/todos/user/";
 export const ADD_TODO = "/todos/add";
