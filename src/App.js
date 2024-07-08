6d640c3c4821ffb8229b96b2bd3cac9f4f8f6672
saurabhB-Webonise <saurabh.bhorkar@weboniselab.com>
2024-07-08 23:38:51 +0530

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Router from './routes/Router';
+import React from "react";
+import Router from "./routes/Router";
 
 function App() {
-  return <Router />
+  return <Router />;
 }
 export default App;
---
+++ src/pages/location/Location.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+
+function Location() {
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+  function success(pos) {
+    var crd = pos.coords;
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+
+    //getLocationInfo(crd.latitude, crd.longitude);
+  }
+
+  function errors(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (result) {
+          console.log(result);
+          if (result.state === "granted") {
+            //If granted then you can directly call your function here
+            navigator.geolocation.getCurrentPosition(success, errors, options);
+          } else if (result.state === "prompt") {
+            //If prompt then the user will be asked to give permission
+            navigator.geolocation.getCurrentPosition(success, errors, options);
+          } else if (result.state === "denied") {
+            //If denied then you have to show instructions to enable location
+          }
+        });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  return <div>location</div>;
+}
+
+export default Location;
